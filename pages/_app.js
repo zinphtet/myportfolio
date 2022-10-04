12239bf9dd9d93aa596efd18d8dc95ce0e20d5b3
Zin PHtet <zinpainghtet.215108@gmail.com>
2022-10-04 11:31:47 +0630

    env variable

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ import Navbar from '../components/Navbar';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-	uri: 'http://localhost:1337/graphql',
+	uri: process.env.NEXT_PUBLIC_GRAPHQL,
 	cache: new InMemoryCache(),
 });
 
